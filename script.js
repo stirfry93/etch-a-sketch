0132db9619426d34d9c4c64212c65623264c5fca
Sterling <sterlingmrichards@gmail.com>
2023-03-16 20:19:35 -0500

    Add hover effect

--- script.js
+++ script.js
@@ -1,28 +1,36 @@
+
 let userChoice = prompt('Enter desired numbers per side of grid (1-100).')
 
-// select container div
 const container = document.querySelector('#container');
-
-// make a for loop that creates many square divs inside of the container
 
 const totalSquares = (userChoice * userChoice);
 
-for (let squares = 0; squares < totalSquares; squares++) {
-    // make a square div (I need 16*16 of these)
+function generateGrid() {
+    for (let squares = 0; squares < totalSquares; squares++) {
     const grid = document.createElement('div');
 
-    // put the square div into the container
     container.appendChild(grid);
-
-    // have JS calculate the necessary height and width to make each div square shaped and fill container
 
     grid.style.height = 500/userChoice + 'px';
     grid.style.width = 500/userChoice + 'px';
 
     grid.style.flexShrink = '0';
 
-    // add border to square
-     grid.style.border = '.5px solid black';
+    grid.style.border = '.5px solid black';
 
+    if (userChoice > 100) {
+        alert("Please choose number between 1-100 instead.")
+        return;
+    }
+}
 }
 
+generateGrid();
+
+const divs = document.querySelectorAll('#container > div');
+
+for (const div of divs) {
+    div.addEventListener('mouseover', function mouseOver() {
+        div.style.backgroundColor = 'black';
+    })
+}
